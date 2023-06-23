702a1be8f17ea4a1ed2117ff15f3b9b1edb701fe
ids21 <grinch.aleks@gmail.com>
2023-06-24 00:25:12 +0300

    test was replaced

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,7 +4,7 @@ from ui.login_page import LoginPage
 from utils.builder import Builder
 
 
-def test_login_with_valid_user(page: Page, login_page: LoginPage) -> None:
+def test_login_with_valid_user(page: Page, login_page: LoginPage):
     user = Builder.create_user()
     login_page.load()
     login_page.login(user.username, user.password)
